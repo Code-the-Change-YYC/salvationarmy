c27d37cf484aad61e9a224eb398ab5bdc0275376
burtonjong <burtonjong05@gmail.com>
2025-11-21 08:45:47 -0700

    SANC-47: placeholder for ci

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -5,9 +5,10 @@ const globalForResend = globalThis as unknown as {
   resend: Resend | undefined;
 };
 
-const apiKey = env.RESEND_API_KEY;
+// need this for ci build
+const apiKey = env.RESEND_API_KEY ?? "placeholder";
 
-const conn = globalForResend.resend ?? new Resend(apiKey ?? "");
+const conn = globalForResend.resend ?? new Resend(apiKey);
 if (env.NODE_ENV !== "production") globalForResend.resend = conn;
 
 export const resend = conn;
