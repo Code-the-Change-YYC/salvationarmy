8d4bc25ddeea1d771d7953f2a2398392617f59f0
burtonjong <burtonjong05@gmail.com>
2025-11-20 23:12:46 -0700

    SANC-47: pls

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -1,4 +1,11 @@
 import { Resend } from "resend";
 import { env } from "@/env";
 
-export const resend = env.RESEND_API_KEY ? new Resend(env.RESEND_API_KEY) : null;
+const globalForResend = globalThis as unknown as {
+  resend: Resend | undefined;
+};
+
+const conn = globalForResend.resend ?? new Resend(env.RESEND_API_KEY);
+if (env.NODE_ENV !== "production") globalForResend.resend = conn;
+
+export const resend = conn;
