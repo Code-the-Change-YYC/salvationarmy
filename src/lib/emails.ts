260b7058b76124c581dc8265685e4dd02c5dcc5c
burtonjong <burtonjong05@gmail.com>
2025-11-20 23:47:51 -0700

    SANC-47: scuff

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -5,7 +5,9 @@ const globalForResend = globalThis as unknown as {
   resend: Resend | undefined;
 };
 
-const conn = globalForResend.resend ?? new Resend(env.RESEND_API_KEY);
+const apiKey = env.RESEND_API_KEY;
+
+const conn = globalForResend.resend ?? new Resend(apiKey ?? "");
 if (env.NODE_ENV !== "production") globalForResend.resend = conn;
 
 export const resend = conn;
