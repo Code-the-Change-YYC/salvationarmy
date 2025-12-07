b4003a310ec5028c4603f5e3e5d0a9eee4670e00
Yemyam <ethan.braum@gmail.com>
2025-12-07 01:25:37 -0800

    add pr review changes

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -5,8 +5,6 @@ import * as authSchema from "@/server/db/auth-schema";
 import * as bookingSchema from "@/server/db/booking-schema";
 import * as postTripSchema from "@/server/db/post-trip-schema";
 import * as schema from "@/server/db/schema";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
