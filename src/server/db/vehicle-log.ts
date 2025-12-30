1dd933bc1c08115b2b61ae693b84ea8f22248a0d
Justin Tan <tanjustin217@gmail.com>
2025-12-30 10:50:46 -0700

    Not null constraint

--- src/server/db/vehicle-log.ts
+++ src/server/db/vehicle-log.ts
@@ -16,9 +16,11 @@ export const logs = pgTable(
     kilometersDriven: integer("kilometers_driven")
       .generatedAlwaysAs((): SQL => sql`${logs.odometerEnd} - ${logs.odometerStart}`)
       .notNull(),
-    driverId: text("driver_id").references(() => user.id, {
-      onDelete: "set null",
-    }),
+    driverId: text("driver_id")
+      .references(() => user.id, {
+        onDelete: "set null",
+      })
+      .notNull(),
     driverName: text("driver_name").notNull(),
     // Name of the vehicle
     vehicle: text("vehicle").notNull(),
