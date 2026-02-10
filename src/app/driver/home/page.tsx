97eba3228ff68bbfab79cf5624a92bca9ff9be2c
Wesley Lui <wesley.lui2@ucalgary.ca>
2026-02-09 20:52:04 -0700

    fix: type check build failure

--- src/app/driver/home/page.tsx
+++ src/app/driver/home/page.tsx
@@ -1,11 +1,11 @@
-import CalendarView from "@/app/_components/common/calendar/calendar-view";
+import CalendarView from "@/app/_components/agencypage/calendar-view";
 import { api } from "@/trpc/server";
 
 export default async function DriverHome() {
   const initialBookings = await api.bookings.getAll();
   return (
     <main>
-      <CalendarView bookings={initialBookings} includeButtons={true}></CalendarView>
+      <CalendarView bookings={initialBookings} />
     </main>
   );
 }
