17b8a44285271ee3ce1a0a01463417f2a0df132b
Wesley Lui <wesley.lui2@ucalgary.ca>
2026-02-11 17:53:46 -0700

    delete drizzle sql file

--- drizzle/0002_colossal_freak.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "post_trip_surveys" ALTER COLUMN "start_reading" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "bookings" ADD COLUMN "survey_completed" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-CREATE INDEX "bookings_start_time_idx" ON "bookings" USING btree ("start_time");
