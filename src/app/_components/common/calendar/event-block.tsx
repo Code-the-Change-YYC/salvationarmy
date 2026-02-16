e68f57995cc9a05f7d842604155d58a1bc4ab8a2
Maxime Boucher <maxime@maximeboucher.com>
2026-02-16 15:12:15 -0700

    SANC-72: Update EventBlock to accept ref prop

--- src/app/_components/common/calendar/event-block.tsx
+++ src/app/_components/common/calendar/event-block.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Group, Text, Tooltip } from "@mantine/core";
 import dayjs from "dayjs";
-import { forwardRef } from "react";
+import type { Ref } from "react";
 
 import { getBookingStatusIcon, getBookingStatusLabel } from "@/lib/utils";
 import type { BookingStatus } from "@/types/types";
@@ -11,39 +11,41 @@ interface EventBlockProps {
   end: Date | null;
   status: BookingStatus | null;
   showTime?: boolean;
+  ref?: Ref<HTMLDivElement>;
 }
 
-const EventBlock = forwardRef<HTMLDivElement, EventBlockProps>(
-  ({ title, start, end, status, showTime = true }, ref) => {
-    const StatusIcon = getBookingStatusIcon(status);
+export default function EventBlock({
+  title,
+  start,
+  end,
+  status,
+  showTime = true,
+  ref,
+}: EventBlockProps) {
+  const StatusIcon = getBookingStatusIcon(status);
 
-    return (
-      <Group ref={ref} p="0.25rem" justify="between" align="start" w="100%">
-        <Box style={{ flex: 1, minWidth: 0 }}>
-          <Text fw={600} size="sm" truncate="end">
-            {title}
+  return (
+    <Group ref={ref} p="0.25rem" justify="between" align="start" w="100%">
+      <Box style={{ flex: 1, minWidth: 0 }}>
+        <Text fw={600} size="sm" truncate="end">
+          {title}
+        </Text>
+        {showTime && start && end && (
+          <Text size="xs" opacity={0.75}>
+            {dayjs(start).format("h:mm")} - {dayjs(end).format("h:mm")}
           </Text>
-          {showTime && start && end && (
-            <Text size="xs" opacity={0.75}>
-              {dayjs(start).format("h:mm")} - {dayjs(end).format("h:mm")}
-            </Text>
-          )}
-        </Box>
+        )}
+      </Box>
 
-        <Box style={{ flex: "0 0 auto" }}>
-          {StatusIcon && (
-            <Tooltip label={getBookingStatusLabel(status)} withArrow>
-              <Flex justify="center" align="center">
-                <StatusIcon size={16} />
-              </Flex>
-            </Tooltip>
-          )}
-        </Box>
-      </Group>
-    );
-  },
-);
-
-EventBlock.displayName = "EventBlock";
-
-export default EventBlock;
+      <Box style={{ flex: "0 0 auto" }}>
+        {StatusIcon && (
+          <Tooltip label={getBookingStatusLabel(status)} withArrow>
+            <Flex justify="center" align="center">
+              <StatusIcon size={16} />
+            </Flex>
+          </Tooltip>
+        )}
+      </Box>
+    </Group>
+  );
+}
